3b65554acf96d9abebb7a377e055cce6096b6463
Awwatts <baptiste.saby@isen.yncrea.fr>
2023-02-14 16:32:42 +0100

    tartiflette

--- app/src/main/java/isen/m1/androiderestaurant/CustomAdapter.kt
+++ app/src/main/java/isen/m1/androiderestaurant/CustomAdapter.kt
@@ -1,5 +1,6 @@
 package isen.m1.androiderestaurant
 
+import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -9,7 +10,9 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import isen.m1.androiderestaurant.databinding.CellCustomBinding
+import isen.m1.androiderestaurant.network.Ingredient
 import isen.m1.androiderestaurant.network.Plate
+import java.io.Serializable
 
 
 class CustomAdapter(val items: List<Plate>, val clickListenner: (Plate) -> Unit):RecyclerView.Adapter<CustomAdapter.CellViewHolder>() {
@@ -37,9 +40,21 @@ class CustomAdapter(val items: List<Plate>, val clickListenner: (Plate) -> Unit)
 		{
 			Picasso.get().load(ItemsViewModel.image[0]).into(holder.imageView)
 		}
+		else
+		{
+			holder.imageView.setImageResource(R.drawable.notfound)
+		}
+
 		holder.root.setOnClickListener {
 			Log.d("button", "click sur un $position")
 			clickListenner(items[position])
+			val intent = Intent(holder.imageView.context, DetailActivity::class.java)
+			intent.putExtra("name", items[position].name)
+			intent.putExtra("ingredients", items[position].ingerdients as Serializable)
+			intent.putExtra("image_id", items[position].image[0])
+			intent.putExtra("price", items[position].price[0].price)
+			holder.imageView.context.startActivity(intent)
+
 		}
 
 	}
--- app/src/main/java/isen/m1/androiderestaurant/DetailActivity.kt
+++ app/src/main/java/isen/m1/androiderestaurant/DetailActivity.kt
@@ -2,7 +2,11 @@ package isen.m1.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
 import isen.m1.androiderestaurant.databinding.ActivityDetailBinding
+import isen.m1.androiderestaurant.network.Ingredient
 
 class DetailActivity : AppCompatActivity() {
 	private lateinit var binding: ActivityDetailBinding
@@ -12,6 +16,47 @@ class DetailActivity : AppCompatActivity() {
 		setContentView(binding.root)
 
 		supportActionBar?.title = "recette"
+		val ingredients = intent.getSerializableExtra("ingredients") as List<Ingredient>
+
+		binding.PlateName.text = intent.getStringExtra("name")
+		binding.IngredientList.text = ingredients.joinToString(separator = " ") { it.name }
+		//binding.PlateImage.setImageResource(intent.getIntExtra("image", 0))
+
+		Log.d("image",intent.getStringExtra("image_id").toString())
+
+		//Affichage de l'image et test si il y a bien une image
+		val imageUrl = intent.getStringExtra("image_id")
+		val imageView = binding.PlateImage
+
+		if (imageUrl != "") {
+			Picasso.get().load(imageUrl).into(imageView)
+		} else {
+			imageView.setImageResource(R.drawable.notfound)
+		}
+
+		binding.addCart.text = "Add for " + intent.getStringExtra("price") + "€"
+
+
 	}
 
+
+	override fun onStart() {
+		super.onStart()
+		Log.d("LifeCycle", "MainActivity onStart")
+	}
+
+	override fun onResume() {
+		super.onResume()
+		Log.d("LifeCycle", "MainActivity onResume")
+	}
+
+	override fun onPause() {
+		super.onPause()
+		Log.d("LifeCycle", "MainActivity onPause")
+	}
+
+	override fun onDestroy() {
+		Log.d("LifeCycle", "MainActivity onDestroy")
+		super.onDestroy()
+	}
 }
